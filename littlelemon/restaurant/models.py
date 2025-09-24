d21847fd96668d858e4b28364b95e6d0dc646d5b
Adidev <anandadidev43@gmail.com>
2025-09-24 14:40:55 +0530

    Setup models and inserted dummy data

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    inventory = models.IntegerField(default=0)
+
+
+class Booking(models.Model):
+    title = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    Booking_date = models.DateTimeField(default=timezone.now)
