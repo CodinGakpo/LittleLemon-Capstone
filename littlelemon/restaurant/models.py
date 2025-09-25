8c0018f812a5077e1a35eda15c0a6a626e70c7d7
Adidev <anandadidev43@gmail.com>
2025-09-26 00:22:51 +0530

    Added serializer

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -6,8 +6,14 @@ class Menu(models.Model):
     price = models.DecimalField(max_digits=10,decimal_places=2)
     inventory = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.title
+
 
 class Booking(models.Model):
     title = models.CharField(max_length=255)
     No_of_guests = models.IntegerField()
     Booking_date = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.title
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateAPIView,DestroyAPIView
+from .models import Menu
+from .serializers import MenuSerializer
 # Create your views here.
+
+
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(DestroyAPIView,RetrieveUpdateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
